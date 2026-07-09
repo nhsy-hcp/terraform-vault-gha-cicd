5a0632349b4290201491c0b87e33a30959744414
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2026-07-09 16:04:54 +0100

    Add tflint file filter and minimum-failure-severity to pre-commit config
    - Scope terraform_tflint hook to bootstrap, namespace-admin, namespace-tn001
    - Add --minimum-failure-severity=warning arg
    - Remove unused vault_addr variable from namespace-admin

--- namespace-admin/variables.tf
+++ namespace-admin/variables.tf
@@ -1,8 +1,3 @@
-variable "vault_addr" {
-  description = "The address of the HCP Vault cluster public endpoint"
-  type        = string
-}
-
 variable "vault_token" {
   description = "The Vault token used to authenticate against the admin namespace"
   type        = string
